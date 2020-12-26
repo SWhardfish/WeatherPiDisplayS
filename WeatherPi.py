ef0f119f72303b77d20996a09b25ab0c94d2b116
anders.rylander2 <arylander@gmail.com>
2020-12-26 22:28:55 +0000

    Updated test setting of 30s refresh of data to 300s.

--- WeatherPi.py
+++ WeatherPi.py
@@ -130,7 +130,7 @@ def main():
                              address)
 
         # start weather forecast thread
-        timer_thread = RepeatedTimer(30, weather_forecast, [
+        timer_thread = RepeatedTimer(300, weather_forecast, [
             config["openweather_appid"], config["latitude"],
             config["longitude"], language, config["units"]
         ])
